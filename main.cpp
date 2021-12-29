a169cb7b805a57d9a68b9fb1ea83a11f86d88ede
Ranaihab <rana.ihab7@gmail.com>
2021-12-29 14:47:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct Node{
+    char data;
+    int idx;
+    int r1;
+    int r2;
+
+public:
+    Node(char d, int i){
+        data = d;
+        idx = i;
+        r1 = 0;
+        r2 = 0;
+    }
+};
+
+class SuffixArray{
+    Node* []nodes;
+public:
+    SuffixArray(String txt){
+        nodes = new Node[txt.size];
+
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
