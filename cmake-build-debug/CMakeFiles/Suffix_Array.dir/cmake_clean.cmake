42c9373a1d5bbef0c780e850504ebfdff386777e
Omar Al Haj <omaralhaj24@gmail.com>
2021-12-29 14:38:44 +0200

    ..

---
+++ cmake-build-debug/CMakeFiles/Suffix_Array.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Suffix_Array.dir/main.cpp.obj"
+  "Suffix_Array.exe"
+  "Suffix_Array.exe.manifest"
+  "Suffix_Array.pdb"
+  "libSuffix_Array.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Suffix_Array.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Suffix_Array.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Suffix_Array.
+# This may be replaced when dependencies are built.
